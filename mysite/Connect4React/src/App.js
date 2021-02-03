39866e500814cce93fed0b1af315fa8fe90ac853
strotman-ryan <strotman.7@osu.edu>
2021-02-03 17:38:56 -0500

    added createing of games for player 2

--- mysite/Connect4React/src/App.js
+++ mysite/Connect4React/src/App.js
@@ -8,8 +8,8 @@ import { isInaccessible } from '@testing-library/react';
 
 //TODO: change the hard coded string
 const game_id = document.getElementById('game_id').textContent;
-const game_link = document.getElementById('game_link').textContent; //this should be like http://0.0.0.0:8000/connect/game/2
-const client = new W3CWebSocket('ws://'+ window.location.host +'/ws/connect4/' + game_id + '/'); //need just ip address and port
+const game_link = document.getElementById('game_link').textContent; //this should be like http://0.0.0.0:8000/game/2
+const client = new W3CWebSocket('ws://'+ window.location.host +'/ws/connect4/' + game_id + '/');
 
 
 class App extends React.Component {
--- mysite/connect4/views.py
+++ mysite/connect4/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 import random
 from django.urls import reverse
 from .models import Connect4Game
 import random
 from datetime import datetime
+
+#Globals
 random.seed(datetime.now())
+session_id_string = 'session_id'
 
 # Create your views here.
 def home(request):
@@ -18,27 +21,46 @@ def creategame(request):
     if request.method == 'POST':
         playerNum = request.POST['player'] #this is a string; either '1' or '2'
         if playerNum not in ['1','2']:
-            raise Http404("Page not available")
-        session_id_string = 'session_id'
-        #create session id if not already have one
-        if not session_id_string in request.session:
-            print('creating seesion_id')
-            request.session[session_id_string] = random.randint(0,2147483647) #max number allowed in database
+            raise Http404("Page not available")      
+        CreateAndAssignSessionId(request)
         game = Connect4Game()
         if playerNum == '1':
             game.player1 = request.session[session_id_string]
         if playerNum == '2':
             game.player2 = request.session[session_id_string]
-        game.save()       
+        game.save()  
+        return redirect('game', game_id = game.id)     
     else:
         raise Http404("Page not available")
-    #create session id if not already have one
-    #create game in db
-    #Assign P1 or P2 to session Id just made
-    #redirect to game with ID
-    return render(request, 'connect4/setup.html')
-    #return redirect('game', game_id = gameNum)
-
+    
+'''
+Check if session Id matches a player
+if it does return game
+if it does not and a player is still null -> assign player to session ID and return
+else 404
+'''
 def game(request, game_id):
-    return render(request, "build/index.html", {'absoluteLink': request.build_absolute_uri(),
-    'game_id': game_id})
+    game = get_object_or_404(Connect4Game, pk=game_id)   
+    if session_id_string in request.session and request.session[session_id_string] in [game.player1, game.player2]:
+        pass #just return the game number
+    elif game.player1 is None:       
+        CreateAndAssignSessionId(request)
+        game.player1 = request.session[session_id_string]
+    elif game.player2 is None:
+        CreateAndAssignSessionId(request)
+        game.player2 = request.session[session_id_string]
+    else:
+        #game is already full
+        #TODO: allow spectators?
+        raise Http404("Page not available")
+    game.save()
+    return render(request, "build/index.html", 
+    {
+        'absoluteLink': request.build_absolute_uri(),
+        'game_id': game_id
+    })
+
+def CreateAndAssignSessionId(request):
+    if session_id_string not in request.session:
+        print('creating seesion_id')
+        request.session[session_id_string] = random.randint(0,2147483647) #max number allowed in database
