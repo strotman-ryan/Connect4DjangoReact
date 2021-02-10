4bdda9b7d5f8900c3805dd3b486e185f0e24748a
strotman-ryan <strotman.7@osu.edu>
2021-02-10 02:00:56 -0500

    made testing possible again with jest

--- mysite/assets/src/Piece.test.js
+++ mysite/assets/src/Piece.test.js
@@ -1,35 +1,36 @@
 import Piece from './Piece'
 import React from 'react'
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
 import '@testing-library/jest-dom'
 
-test('Piece OnePlayerPiece', async () =>{
+
+
+test('Piece OnePlayerPiece', () =>{
     const {getByTestId} = render(<Piece value={1}/>);
     expect(getByTestId("piece")).toHaveClass("piece");
     expect(getByTestId("piece")).toHaveClass("player-one-piece");
 })
 
 
-test('Piece TwoPlayerPiece', async () =>{
+test('Piece TwoPlayerPiece', () =>{
     const {getByTestId} = render(<Piece value={2}/>);
     expect(getByTestId("piece")).toHaveClass("piece");
     expect(getByTestId("piece")).toHaveClass("player-two-piece");
 })
 
-test('Piece EmptyPiece', async () =>{
+test('Piece EmptyPiece', () =>{
     const {getByTestId} = render(<Piece value={0}/>);
     expect(getByTestId("piece")).toHaveClass("piece");
     expect(getByTestId("piece")).toHaveClass("open-piece");
 })
 
-test('Piece Possible 1 piece', async () =>{
+test('Piece Possible 1 piece', () =>{
     const {getByTestId} = render(<Piece value={3}/>);
     expect(getByTestId("piece")).toHaveClass("piece");
     expect(getByTestId("piece")).toHaveClass("player-one-piece-possible");
 })
 
-test('Piece Possible 2 piece', async () =>{
+test('Piece Possible 2 piece', () =>{
     const {getByTestId} = render(<Piece value={4}/>);
     expect(getByTestId("piece")).toHaveClass("piece");
     expect(getByTestId("piece")).toHaveClass("player-two-piece-possible");
